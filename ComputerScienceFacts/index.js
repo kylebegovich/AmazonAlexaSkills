5b599072548113185f6ac80e8df2af13bd5b68d0
begovic2 <begovic2@illinois.edu>
2017-07-11 14:48:33 -0500

    Let's me mature, now

--- ComputerScienceFacts/index.js
+++ ComputerScienceFacts/index.js
@@ -8,36 +8,17 @@ var SKILL_NAME = 'Computer Science Facts';
  * Array containing cs facts.
  */
 var CS_FACTS = [
-    "Computer Science is better than Electrical and Computer Engineering.",
     "Computer Science is lit.",
     "Java, while also another name for delicious programmer juice, coffee, is a programming language.",
-    "Computer Science draws a lot of its algorithms and problem solving from Mathematics.",
     "The word computer, before its common denotation, referred to an individual who did computations. Not a machine.",
-    "The most common virus on computers is called Windows and affects over 90 percent of user machines.",
-    "The meme community participation and the popularity of computer science as a major have a concerning level of similarity.",
-    "VIM has self-esteem issues, as well it should with how bad it is sometimes.",
+    "The most common virus on computers is called Windows and affects over 50 percent of user machines.",
     "Almost twice as many people can write java on the planet than can write Finnish.",
-    "Kyle knows how to code, and therefore is a computer scientist, and therefore slays.",
-    "Hackathons are coding marathons Computer scientists submit themselves to for some reason.",
-    "A Computer Science degree can lead to a number of fields, anything from software engineer to applied mathematician.",
+    "An algorithm is a precise method usable by a computer for the solution to a problem.",
     "Computer Science has a lot of facts about it, and therefore makes for great hoodie materials.",
-    "Recursion is when a function calls recursion is when a function calls recursion is when a function calls Stack Overflow.",
-    "Computer science is often abbreviated to c s.",
-    "Research mathematics relys heavily on the coding ability of computer scientists.",
-    "Programming and computer science, while closely related, are not synonyms.",
-    "Computer engineers and computer scientists work closely together, despite being mortal enemies.",
-    "The computer scientist is much like the humble bumblebee, neither should be able to fly.",
-    "The computer scientist is much like the philosopher, both ask impossible questions often.",
-    "The computer scientist is much like the mathematician, both are party animals.",
-    "The computer scientist is much like the university staff, neither knows what they're doing most of the time.",
-    "The computer scientist is much like the meme community, but that's none of my business",
-    "The computer scientist is much like the meme community, but that's where you're wrong kid oh.",
-    "The computer scientist is much like java, both spend way too much time figuring everything out when a solution is simple.",
-    "The computer scientist is much like python, both don't write their own work half the time.",
-    "You don't have permission to access the joke about UNIX Shell",
-    "The computer scientist is much like HTML, neither flowerpot can, web server link, button, click, welcome back!",
-    "The computer scientist is much like assembly, both are way too careful about defining every little aspect of their work",
-    "The computer scientist is much like the terminal window, neither is particularly fancy but are extremly useful"
+    "Computer science was not founded solely by mathematicians. An equally important group was electrical engineers, who focused on actually building a computing machine.",
+    "Research mathematics rely's heavily on the coding ability of computer scientists.",
+    "Programming and computer science, while closely related, are not synonymous.",
+    "Computer engineers and computer scientists work closely together, despite being mortal enemies."
 ];
 
 exports.handler = function(event, context, callback) {
